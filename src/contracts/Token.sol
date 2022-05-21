fee8c3c0918ced5d2d3e451212c789ef378aac29
David Nii Armah <davidarmah991@gmail.com>
2022-05-21 02:12:23 +0000

    Add files via upload

---
+++ src/contracts/Token.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+pragma solidity  >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract Token is ERC20 {
+  address public minter;
+
+  event MinterChanged(address indexed from, address to);
+
+  constructor() public payable ERC20("Decentralized Bank Currency", "DBC") {
+  minter = msg.sender;
+  }
+
+
+function passMinterRole(address dBank) public returns(bool) {
+  require(msg.sender == minter, 'Error, only owner can change pass minter role');
+
+  emit MinterChanged(msg.sender, dBank);
+  return true;
+}
+
+function mint(address account, uint256 amount) {
+  require(msg.sender == minter, 'Error, msg.sender ');
+  _mint(account, amount);
+}
+}
